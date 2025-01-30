fa7bf5e219f23a92aff3f53b3b63581b58426763
LuizWT <luizfelipe.sec@proton.me>
2025-01-30 16:41:08 -0300

    update

--- Scanner.py
+++ Scanner.py
@@ -157,7 +157,7 @@ async def dns_resolver():
    / , _/ -_|_-< _ \/ / |/ / -_) __/
   /_/|_|\__/___|___/_/|___/\__/_/   
                                    """)
-    print('{:^50}'.format(f'{COLORS["Twhite"]}━━━━━━━━━━┫ @LuizWT on Discord'.center(50)))
+    print('{:^50}'.format(f'{COLORS["Twhite"]}━━━━━━━━━━┫ @LuizWT'.center(50)))
     
     while True:
         alvo = input(f'\n{COLORS["Dgreen"]}Digite o Domínio para DNS (ex: example.com):{COLORS["VRCRM"]} ').strip()
@@ -218,7 +218,7 @@ def main():
    _\ \/ __/ _ `/ _ \/ _ \/ -_) __/
   /___/\__/\_,_/_//_/_//_/\__/_/   
                                    """)
-            print(f'{COLORS["Twhite"]}━━━━━━━━━━┫ @LuizWT on Discord'.center(50))
+            print(f'{COLORS["Twhite"]}━━━━━━━━━━┫ @LuizWT'.center(50))
             
             while True:
                 alvo = input(f'\n{COLORS["Dgreen"]}Digite o IP/Domínio:{COLORS["VRCRM"]} ').strip()
